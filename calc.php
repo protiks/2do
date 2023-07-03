40c867479cb73e6f90046246fc86159b854249c9
protiks <87400391+protiks@users.noreply.github.com>
2023-07-03 18:50:26 +0200

    update calc

--- calc.php
+++ calc.php
@@ -1,12 +1,33 @@
 <?php
-echo "Enter a number\n";
-$num1= fgets(STDIN);
+$validNum1 = false;
+$validNum2 = false;
+$num1 = null;
+$num2 = null;
 
-// echo "Enter another one\n";
+while (!$validNum1 || !$validNum2) {
+    if (!$validNum1) {
+        echo "Enter a number: ";
+        $inputNum1 = trim(fgets(STDIN));
+        if (is_numeric($inputNum1)) {
+            $validNum1 = true;
+            $num1 = $inputNum1;
+        } else {
+            echo "Please enter a valid number.\n";
+        }
+    }
 
-$num2 = fgets(STDIN);
+    if (!$validNum2) {
+        echo "Enter another number: ";
+        $inputNum2 = trim(fgets(STDIN));
+        if (is_numeric($inputNum2)) {
+            $validNum2 = true;
+            $num2 = $inputNum2;
+        } else {
+            echo " Please enter a valid number.\n";
+        }
+    }
+}
 
-$result = $num1 + $num2;
-
-echo "Result $result\n";
+$result = $num1 * $num2;
+echo "Result: $result\n";
 ?>
